6b50e29b5375c0d9cb38bdce569dac75aae66eec
Luis Gomes <luisgomesbcc@gmail.com>
2020-08-07 18:16:02 -0300

    Projeto Mobile

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}> App das Aliciãs</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#082',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: 'bold',
+    transform: [
+      {rotateY: '30deg'}
+    ]
+  }
 });
